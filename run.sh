40a350400902c2305d03e4d8880e664f27be75a7
Kehina Manseri <manserikehina@gmail.com>
2026-04-11 09:13:40 +0200

    :zap: removing the log from the command bc bon.

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ PRIMARY=$1
 BACKFILLING=$2
 THRESHOLD=$3
 
-# To ensure we give all needed args
 if [[ $# -ne 3 ]]; then
     echo "PRIMARY, BACKFILLING and THRESHOLD FLAG must be provided"
     echo "Usage: ./run.sh PRIMARY BACKFILLING THRESHOLD"
@@ -19,12 +18,12 @@ mkdir -p "$OUTPUTDIR"
 mkdir -p "$LOGDIR"
 
 echo "Simulation will run with ${PRIMARY} policy and ${BACKFILLING} policy!"
-echo "Threshold flag is ${THRESHOLD}."
+echo "Threshold flag is ${THRESHOLD}!"
 echo "Logs will be saved in ${LOGDIR} and outputs in ${OUTPUTDIR}."
 
 batsim \
   -p assets/plateform.xml \
   -w assets/testInput.json \
   -l ./build/libeasy_tuning.so "$PRIMARY $BACKFILLING $THRESHOLD" \
-  -e "$OUTPUTDIR/" \
-  > "$LOGDIR/log.txt"
+  -e "$OUTPUTDIR/" 
+#   > "$LOGDIR/log.txt"
